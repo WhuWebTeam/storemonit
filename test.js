c8b65d0f197ad37f826b415d50ec57e05e8b3391
qwasxj <1942750262@qq.com>
2017-12-21 10:12:26 -0500

    temp

--- test.js
+++ test.js
@@ -1,68 +1,19 @@
+const cluster = require('cluster');
+const http = require('http');
+const cpus = require('os').cpus();
 
-count1 = [
-    {
-        checkerid: "1",
-        checkername: "xj",
-        count: "2"
+if (cluster.isMaster) {
+
+    cosole.log(`master ${process.pid} is running...`);
+
+    for (let i = 0; i < cpus.length; i++) {
+        cluster.fork();
     }
-],
-count2 = [
-    {
-        checkerid: "1",
-        checkername: "xj",
-        count: "1"
-    },
-    {
-        checkerid: "2",
-        checkername: "jj",
-        count: "1"
-    }
-],
-count3 = []
+} else {
 
-
-
-const temp = {};
-count1.map(obj => {
-    if (!temp[obj.checkerid]) {
-        temp[obj.checkerid] = {
-            checkerId: obj.checkerid,
-            checkerName: obj.checkername,
-            count1: +obj.count,
-            count2: 0,
-            count3: 0,
-        }
-    }
-});
-
-count2.map(obj => {
-    if (!temp[obj.checkerid]) {
-        temp[obj.checkerid] = {
-            checkerId: obj.checkerid,
-            checkerName: obj.checkername,
-            count1: 0,
-            count2: +obj.count,
-            count3: 0
-        }
-    } else {
-        temp[obj.checkerid].count2 =  +obj.count;
-    }
-});
-
-
-count3.map(obj => {
-    if (!temp[obj.checkerid]) {
-        temp[obj.checkerid] = {
-            checkerId: obj.checkerid,
-            checkerName: obj.checkername,
-            count1: 0,
-            count2: 0,
-            count3: +obj.count,
-        }
-    } else {
-        temp[obj[checkerid]].count3 =  +obj.count;
-    }
-});
-
-
-console.log(Object.values(temp));
+    http.createServer((req, res) => {
+        res.writeHead(200);
+        res.write(`${process.pid}`);
+        res.end();
+    }).listen(8000);
+}
