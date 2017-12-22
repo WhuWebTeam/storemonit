f60f1357aa5f7f6e7efdd2b9a498fff66f1c4f32
qwasxj <1942750262@qq.com>
2017-12-22 09:53:52 -0500

    temp

--- app/controller/index.js
+++ app/controller/index.js
@@ -54,7 +54,7 @@ module.exports = app => {
             const tables = [
                 'authorities', 'counterUser', 'counters', 'shops', 'areas', 'products', 'customers', 'shopUser',
                 'cashiers', 'bills', 'eventsList', 'cashierSalesInfo', 'customerSalesInfo', 'productSalesInfo', 'eventTAT',
-                'users', 'userswm', 'counterTypeConf'
+                'users', 'counterTypeConf'
             ];
 
             tables.map(async table => {
