8baffc9a78908859146dbff38239f58f96805471
LLemily <498594396@qq.com>
2018-01-02 18:23:25 +0800

    ss

--- app/public/js/func.js
+++ app/public/js/func.js
@@ -23,7 +23,7 @@
         if (hasClass(elem, cls)) {
             var newClass = ' ' + elem.className.replace(/[\t\r\n]/g, '') + ' ';
             while (newClass.indexOf(' ' + cls + ' ') >= 0) {
-            newClass = newClass.replace(' ' + cls + ' ', ' ');
+                newClass = newClass.replace(' ' + cls + ' ', ' ');
             }
             elem.className = newClass.replace(/^\s+|\s+$/g, '');
         }
