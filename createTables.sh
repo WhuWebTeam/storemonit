7e5f958ab69dd476eb4ac31e7f7c93dbfb9cacd4
qwasxj <1942750262@qq.com>
2018-01-03 09:21:29 +0800

    complete table sql script

--- createTables.sh
+++ createTables.sh
@@ -98,6 +98,18 @@ ALTER TABLE public.counters
   OWNER TO company;
 
 
+CREATE TABLE public.counterTypeConf
+(
+    id varchar(50) primary key,
+    type varchar(50)
+) 
+WITH (
+  OIDS = FALSE
+);
+ALTER TABLE public.counterTypeConf
+  OWNER TO company;
+
+
 CREATE TABLE public.shopUser
 (
     id serial primary key,
@@ -118,7 +130,8 @@ CREATE TABLE public.shops
     id varchar(50) primary key,
     areaId varchar(50),
     name varchar(50),
-    details varchar(500)
+    details varchar(500),
+    type varchar(50)
 )
 WITH (
   OIDS = FALSE
